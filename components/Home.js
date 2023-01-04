3f92827942dd02eeec99438ff06d0f2f1faefc3e
Hadas Axelbaum <hadasa@inmedmd.com>
2023-01-04 14:23:34 +0200

    create basice homeScreen ui

--- components/Home.js
+++ components/Home.js
@@ -1,7 +1,49 @@
-import {Text, View} from 'react-native';
+import {Text, View, StyleSheet, Button} from 'react-native';
+
+const completeSets = 1;
+const itemsChosen = 3;
+const itemsToChoose = 3;
+const isDressComplete = itemsChosen === itemsToChoose;
 export default HomeScreen = () => (
-  <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-    <Text category="h1">HOME</Text>
-    {/* <Button>Shirts</Button> */}
-  </View>
+  <>
+    <Text style={styles.titleText}>Application</Text>
+
+    <View style={styles.view}>
+      <Text style={styles.bodyText} category="h1">
+        Sets completed: {completeSets}
+      </Text>
+      <Text style={styles.bodyText} category="h1">
+        Dress stattus: {itemsChosen} / {itemsToChoose}
+      </Text>
+      {isDressComplete && (
+        <Button
+          title="Done"
+          style={styles.button}
+          color="#f194ff"
+          onPress={() => console.log('Button with adjusted color pressed')}
+        />
+      )}
+    </View>
+  </>
 );
+
+const styles = StyleSheet.create({
+  view: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  titleText: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    alignSelf: 'center',
+    margin: 20,
+  },
+  bodyText: {
+    fontSize: 15,
+    margin: 5,
+  },
+  button: {
+    margin: 5,
+  },
+});
