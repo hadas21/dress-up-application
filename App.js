b8cd9e362a2c4d827dccd2c1e65df2c04a9772ac
Hadas Axelbaum <hadasa@inmedmd.com>
2023-01-05 21:26:51 +0200

    extract sideNavigator to its own component

--- App.js
+++ App.js
@@ -1,25 +1,13 @@
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createDrawerNavigator} from '@react-navigation/drawer';
 import {Provider} from 'react-redux';
 import {Store} from './assets/redux/store';
-
-import HomeScreen from './components/Home';
-import ShirtsScreen from './components/Shirts';
-import PantsScreen from './components/Pants';
-import ShoesScreen from './components/Shoes';
-
-const Drawer = createDrawerNavigator();
+import SideNavigator from './navigation/SideNavigator';
+import {NavigationContainer} from '@react-navigation/native';
 
 export default App = () => (
   <Provider store={Store}>
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={HomeScreen} />
-        <Drawer.Screen name="Shirts" component={ShirtsScreen} />
-        <Drawer.Screen name="Pants" component={PantsScreen} />
-        <Drawer.Screen name="Shoes" component={ShoesScreen} />
-      </Drawer.Navigator>
+      <SideNavigator />
     </NavigationContainer>
   </Provider>
 );
--- components/DisplayItems.js
+++ components/DisplayItems.js
@@ -84,6 +84,8 @@ export default DisplayItems = props => {
     </Modal>
   );
 
+  console.log('BBBBBBBBBBBBBBB', data);
+
   return (
     <>
       <SafeAreaView style={styles.container}>
